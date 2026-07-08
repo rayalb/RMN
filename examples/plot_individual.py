8f955a915f2e9205aacacacb2805aa56d5f1dafc
RayAlbert <raymundo.albert@gmail.com>
2026-07-08 15:08:40 -0300

    change path from house to INTI

--- examples/plot_individual.py
+++ examples/plot_individual.py
@@ -3,17 +3,19 @@ Example:
 
 Load an individual spectrum and plot it.
 """
-
+import os 
 from pathlib import Path
 
 from SimulacionRMN.library import SpectrumLibrary
 
 # Path
 
-root_dir = Path("/home/Documents/")
+#root_dir = Path("/home/ray/Documents/") # CASA
+root_dir = Path("/home/ray/Documents/Quimica/") #INTI
 
-metadata = (root_dir, "RMN", "metadata_espectros_individuales_con_TSP sodico(SI).xlsx")
-spectra = (root_dir, "RMN", "Espectros individuales + TSP sodico (SI)")
+
+metadata = os.path.join(root_dir, "DataSet", "metadata_espectros_individuales_con_TSP sodico(SI).xlsx")
+spectra = os.path.join(root_dir, "Espectros individuales + TSP sodico (SI)")
 
 # Load Library
 
@@ -26,7 +28,7 @@ print(library.compounds()[:10])
 
 # Load one spectrum
 
-spec = library.get_raw("Glucose")
+spec = library.get_raw("glucosa")
 print(spec)
 
 print(spec.metadata)
