6237c02409fb095162edc25ffdd8edab5466e1fe
ray albert <raymundo.albert@gmail.com>
2026-07-09 11:03:33 -0300

    generate and plotting mixtures example

---
+++ examples/plot_mixture.py
@@ -0,0 +1,40 @@
+"""
+Example
+Load several spetrums and mix them
+"""
+
+import os
+from pathlib import Path
+
+from SimulacionRMN.library import SpectrumLibrary
+from SimulacionRMN.mixture import MixtureSimulator
+from SimulacionRMN.plotting import plot_mixture
+
+
+root_dir = Path("/home/ray/Documents/RMN/") # CASA
+#root_dir = Path("/home/ray/Documents/Quimica/") # INTI
+
+metadata = os.path.join(root_dir, "DataSet", "metadata_espectros_individuales_con_TSP sodico(SI).xlsx")
+spectra = os.path.join(root_dir, "Espectros individuales + TSP sodico(SI)")
+
+library = SpectrumLibrary(metadata_path = metadata, spectra_dir = spectra)
+
+simulator = MixtureSimulator(library)
+
+mixture = simulator(["glucosa", "fructosa", "sacarosa"], proportions = [0.5, 0.3, 0.2], 
+                    normalize_concentrations = True)
+
+print(mixture)
+print(mixture.compositions)
+plot_mixture(mixture)
+
+
+# Random mixtures
+
+sim_random_mixtures = MixtureSimulator(library, random_state = 42)
+mixtures = sim_random_mixtures(n_compounds = 4, normalize_concentrations = True)
+
+print(mixtures.compositions)
+plot_mixture(mixtures)
+
+                               
